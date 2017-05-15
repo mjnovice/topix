10fe4451d72cfe2d25689661dc9bc02e0c18db5a
mjnovice <mayank25080562@gmail.com>
2017-05-15 15:08:41 +0530

    Adding topic_store data structure and its methods.

---
+++ topic_store/topic_store.go
@@ -0,0 +1,98 @@
+package topic_store
+
+import (
+	"errors"
+)
+
+type TopicStoreElement struct {
+	Text      string
+	UpVotes   int64
+	DownVotes int64
+}
+
+type TopicStore struct {
+	topics        []TopicStoreElement
+	hotTopics     map[int]int64
+	hotTopicLimit int
+	access        chan bool
+}
+
+func NewTopicStore(hotTopicLimit int) *TopicStore {
+	t := &TopicStore{
+		hotTopics:     make(map[int]int64),
+		access:        make(chan bool, 1),
+		hotTopicLimit: hotTopicLimit,
+	}
+	t.access <- true
+	return t
+}
+
+func (t *TopicStore) validateTopicId(id int) bool {
+	if (id < 0) || (id > len(t.topics)-1) {
+		return false
+	}
+	return true
+}
+
+func (t *TopicStore) Insert(topicText string) {
+	topic := TopicStoreElement{
+		Text:      topicText,
+		UpVotes:   0,
+		DownVotes: 0,
+	}
+	<-t.access
+	//Entering critical section
+	t.topics = append(t.topics, topic)
+	lastInsertId := len(t.topics) - 1
+
+	//case where there are no hot topics yet.
+	if len(t.hotTopics) < t.hotTopicLimit {
+		t.hotTopics[lastInsertId] = topic.UpVotes
+	}
+	//Exiting critical section
+	t.access <- true
+}
+
+func (t *TopicStore) GetTopics() []TopicStoreElement {
+	return t.topics
+}
+
+func (t *TopicStore) UpVote(currentTopicId int) error {
+	if !t.validateTopicId(currentTopicId) {
+		return errors.New("Bad topic id")
+	}
+	t.topics[currentTopicId].UpVotes += 1
+	<-t.access
+	//Entering critical section
+	//updating hot topics
+	for topicId, numUpVotes := range t.hotTopics {
+		if numUpVotes < t.topics[topicId].UpVotes {
+			delete(t.hotTopics, topicId)
+			t.hotTopics[currentTopicId] = t.topics[currentTopicId].UpVotes
+			break
+		}
+	}
+	//Exiting critical section
+	t.access <- true
+	return nil
+}
+
+func (t *TopicStore) DownVote(currentTopicId int) error {
+	if !t.validateTopicId(currentTopicId) {
+		return errors.New("Bad topic id")
+	}
+	t.topics[currentTopicId].DownVotes += 1
+	return nil
+}
+
+func (t *TopicStore) GetHotTopics() []TopicStoreElement {
+	result := []TopicStoreElement{}
+	<-t.access
+	//Entering critical section
+	for topicId, _ := range t.hotTopics {
+		result = append(result, t.topics[topicId])
+	}
+	//Exiting critical section
+	t.access <- true
+	return result
+}
---
+++ topic_store/topic_store_test.go
@@ -0,0 +1,50 @@
+package topic_store
+
+import (
+	"fmt"
+	"testing"
+)
+
+var hotTopicLimit = 20
+var topicStore = NewTopicStore(hotTopicLimit)
+
+func TestInsert(t *testing.T) {
+	topicStore.Insert("hello")
+	if topicStore.topics[0].Text != "hello" {
+		t.Error("Insert failed.")
+	}
+}
+
+func TestUpVoteUpVote(t *testing.T) {
+	topicStore.UpVote(0)
+	if topicStore.topics[0].UpVotes != 1 {
+		t.Error("Upvote failed.")
+	}
+}
+
+func TestUpVoteDownVote(t *testing.T) {
+	topicStore.DownVote(0)
+	if topicStore.topics[0].DownVotes != 1 {
+		t.Error("Downvote failed.")
+	}
+}
+
+func TestGetAllTopics(t *testing.T) {
+	topics := topicStore.GetTopics()
+	if !(len(topics) == 1 && topics[0].Text == "hello") {
+		t.Error("GetAllTopicsFailed")
+	}
+}
+
+func TestGetHotTopics(t *testing.T) {
+	topics := topicStore.GetHotTopics()
+	if !((len(topics) == 1) && (topics[0].Text == "hello")) {
+		t.Error("GetHotTopicsFailed")
+	}
+}
+
+func TestValidateTopicId(t *testing.T) {
+	if !(topicStore.validateTopicId(-1) == false && topicStore.validateTopicId(1) == false) {
+		t.Error("ValidateTopicId failed.")
+	}
+}
